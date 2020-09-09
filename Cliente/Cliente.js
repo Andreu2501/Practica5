b96cbe0d8eb9ce888d06e03ea4773df7abc05016
Andreu2501 <andreaflores2501@gmail.com>
2020-09-08 20:43:23 -0600

    api verificar el estado del pedido

--- Cliente/Cliente.js
+++ Cliente/Cliente.js
@@ -18,3 +18,20 @@ app.get('/GetOrden', (req,res)=>{
     l_axios.post('http://localhost:8003/Restaurante/PostOrden',{'id':noOrden});
     res.json({'noOrden':noOrden});    
 })
+
+/**Verificar el estado del pedido */
+app.get('/GetEstadoPedido',body,function(req,res){
+    var order = req.body.id;
+    console.log(order);
+    var descripcion = "Se desea saber el estado de orden";
+    console.log(descripcion);
+    l_axios.get('http://localhost:8003/Restaurante/GetEstado/'+order)
+        .then(function(response){
+            res.send(response['data']);
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+        .then(function () {
+        });
+});
